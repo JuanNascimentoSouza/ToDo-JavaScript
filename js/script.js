48fd7f0676625e6b1e348eed44d2bc2758dd4b42
Juan <104234658+JuanNascimentoSouza@users.noreply.github.com>
2023-01-10 11:36:12 -0300

    javascript filter

--- js/script.js
+++ js/script.js
@@ -28,9 +28,91 @@ const saveTodo = (text, done = 0, save = 1) => {
     const editBtn = document.createElement("button");
     editBtn.classList.add("edit-todo");
     todo.appendChild(doneBtn);
-    
+
     const deleteBtn = document.createElement("button");
     deleteBtn.classList.add("remove-todo");
     deleteBtn.innerHTML = '<i class="fa-solid fa-xmark"></i>';
     todo.appendChild(deleteBtn)
+
+    //Utilizando dados da localStorage
+    if (done) {
+        todo.classList.add("done");
+    }
+
+    if (save) {
+        saveLocalStorage({ text, done: 0 });
+    }
+
+    todoList.appendChild(todo);
+
+    todoInput.value = "";
+};
+
+const toggleForms = () => {
+    editForm.classList.toggle("hide");
+    todoForm.classList.toggle("hide");
+    todoList.classList.toggle("hide");
+};
+
+const updateTodo = (text) => {
+    const todos = document.querySelectorAll(".todo");
+
+    todos.forEach((todo) => {
+        let todoTittle = todo.querySelector(h3);
+
+        if (todoTittle.innerText === oldInputValue) {
+            todoTitle.innerText = text;
+
+            updateTodoLocalStorage(oldInputValue, text)
+        }
+    });
+};
+
+const getSearchedTodos = (search) => {
+    const todos = document.querySelectorAll(".todos");
+
+    todos.forEach((todo) => {
+        const todoTitle = todo.querySelector("h3").innerText.toLowerCase();
+
+        todo.getElementsByClassName.display = "flex";
+
+        console.log(todoTitle);
+
+        if (!todoTitle.includes(search)) {
+            todo.getElementsByClassName.display = "none";
+        }
+
+    });
 }
+
+const filterTodos = (filterValue) => {
+    const todos = document.querySelectorAll(".todo");
+
+    switch (filterValue) {
+        case "all":
+            todos.forEach((todo) => (todo.style.display = "flex"));
+
+            break;
+
+        case "done":
+            todos.forEach((todo) =>
+                todo.classList.contains("done")
+                    ? (todo.style.display = "flex")
+                    : (todo.style.display = "none")
+            );
+
+            break;
+
+        case "todo":
+            todos.forEach((todo) =>
+                !todo.classList.contains("done")
+                    ? (todo.style.display = "flex")
+                    : (todo.style.display = "none")
+            );
+
+            break;
+
+        default:
+            break;
+    }
+};
